de7f7c19855b4397af32eb79d4efbfe985249500
Eduardo Perez <eapv28@hotmail.com>
2025-08-30 17:27:24 -0400

    First Part

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE employees(
+
+id SERIAL PRIMARY KEY,
+name TEXT NOT NULL,
+birthday DATE NOT NULL,
+salary INTEGER NOT NULL
+
+);
+
 
--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,6 @@
 import db from "#db/client";
-
+import { faker }from "@faker-js/faker";
+import { createEmployee } from "#db/queries/employees";
 await db.connect();
 await seedEmployees();
 await db.end();
@@ -7,4 +8,12 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+    for (let i = 0 ; i < 10 ; i++){
+    const employee ={
+      name: faker.music.artist(),
+     birthday: faker.date.past(),
+      salary: faker.number.int({min: 10 , max: 100}),
+    }
+    await createEmployee(employee);
+  }
 }
